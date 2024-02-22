3da8b14f84f107864c1f3879347b52bb70306730
Blessed Oigbochie <eprince1800@gmail.com>
2024-02-22 14:47:34 +0100

    updated app.py to handle 404 errors and display a 404 json error

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,13 @@ def teardown_appcontext(arg=None):
     """
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    404_json = {
+            "error": "Not found"
+            }
+    return 404_json, 404
+
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.environ.get('HBNB_API_PORT', 5000))
