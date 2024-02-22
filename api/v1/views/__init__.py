c749b7203620942c83e8ed3ae4109c0501fd4c04
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-22 20:45:29 +0100

    task 6: added a new view for State objects that handles all default RESTFul API actions: api/v1/views/__init__.py api/v1/views/states.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from . import states
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
---
+++ api/v1/views/states.py
@@ -0,0 +1,42 @@
+from flask import jsonify, request
+from api.v1.views import app_views
+from models.state import State
+
+@app_views.route("/states", methods=['GET', 'POST'])
+def for_states():
+    if request.method == 'GET':
+        states = State.all()
+        return [State.to_dict() for state in states]
+    elif request.method == 'POST':
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Not a JSON"}), 400
+        if 'name' not in data:
+            return jsonify({"error": "Missing name"}), 400
+        new_state = State(**data)
+        new_state.save()
+        return new_state.to_dict(), 201
+    
+@app_views.route('/states/<state_id>', methods=['GET', 'PUT','DELETE'])
+def for_state(state_id):
+    state = State.get(state_id)
+    if not state:
+        return jsonify({"error": "State not found"}), 404
+    
+    if request.method == 'GET':
+        return State.to_dict()
+    
+    elif request.method == 'PUT':
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Not a JSON"}), 400
+        for key, value in data.items():
+            if key not in ['id', 'created_at', 'uptated_at']:
+                setattr(state, key, value)
+        state.save()
+        return State.to_dict(), 200
+    
+    elif request.method == 'DELETE':
+        state.delete()
+        return jsonify({}), 200
+    
