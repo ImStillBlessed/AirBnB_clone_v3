90677b91e3abf44f5fd1e1d57f4cd598d8f6183d
Blessed Oigbochie <eprince1800@gmail.com>
2024-02-22 12:56:44 +0100

    completed the task 4 for index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,11 @@
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route("/status", strict_slashes=False)
 def status():
@@ -9,4 +15,13 @@ def status():
     return output
 
 @app_views.route("/api/v1/stats", strict_slashes=False)
-def 
+def count_obj():
+    json_obj = {
+    "amenities": storage.count(Amenity),
+    "cities": storage.count(City),
+    "places": storage.count(Place),
+    "reviews": storage.count(Review),
+    "states": storage.count(State),
+    "users": storage.count(User)
+    }
+    return json_obj
