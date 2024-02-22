e30cf40d5529b14cd9a500766b93e3905f3fc8ba
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-22 12:15:21 +0100

    completed the task 3 evironment variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,5 +14,6 @@ def teardown_appcontext(arg=None):
     storage.close()
 
 if __name__ == "__main__":
-    # app.run(host=HBNB_API_HOST, port=HBNB_API_PORT)
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
