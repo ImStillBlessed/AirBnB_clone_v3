84e6142c8d57100fad6cb319f569652fc5335363
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-23 20:18:43 +0100

    total refactoring for places_reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -10,8 +10,8 @@ from models.review import Review
 def get_review_by_place(place_id):
     place = storage.get("Place", place_id)
     return jsonify([review.to_dict() for review in place.review]) if place else abort(404)
-@app_views.route("/reviews/<review_id>", methods=['GET'], strict_slashes=False)
 
+@app_views.route("/reviews/<review_id>", methods=['GET'], strict_slashes=False)
 def get_review(review_id):
     review = storage.get("Review", review_id)
     return jsonify(review.to_dict()) if review else abort(404)
@@ -29,8 +29,8 @@ def delete_review(review_id):
 def create_review(place_id):
     place = storage.get("Place", place_id)
     data = request.get_json()
-    if not (place and data and "user_id" in data and "name" in data and storage.get("User", data["user_id"])):
-        abort(400, 'Invalid JSON or missing user_id/name')
+    if not (place and data and "user_id" in data and "text" in data and storage.get("User", data["user_id"])):
+        abort(400, 'Invalid JSON or missing user_id/text')
     data["place_id"] = place_id
     review = Review(**data)
     review.save()
@@ -43,7 +43,7 @@ def update_review(review_id):
     if not (review and data):
         abort(404 if not review else 400, 'Invalid JSON')
     for key, value in data.items():
-        if key not in ["id", "user_id", "city_id", "created_at", "updated_at"]:
+        if key not in ["id", "user_id", "place_id", "created_at", "updated_at"]:
             setattr(review, key, value)
     review.save()
     return jsonify(review.to_dict()), 200
